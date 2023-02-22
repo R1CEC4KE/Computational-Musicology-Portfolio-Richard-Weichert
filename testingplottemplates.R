61c5cbca166d6177db0bb0b9c98d9f8501a1acf8
R1CEC4KE <richard.weichert@gmail.com>
2023-02-22 17:58:36 +0100

    crying

--- testingplottemplates.R
+++ testingplottemplates.R
@@ -1,7 +1,7 @@
-plotDGDpop + geom_point(aes(size = track.popularity), 
-                        fill = alpha(mode_name, 0.5), 
-                        colour = alpha(track.album.name, 1), 
-                        shape = 23, stroke = 2) + 
+plotDGDpop + geom_point(aes(size = track.popularity, 
+                        fill = mode_name, 
+                        colour = track.album.name), 
+                        shape = 23, stroke = 2, alpha = 0.5) + 
   theme(plot.title = element_text(face = "bold", size = 14),
         plot.caption = element_text(face = "italic", size = 9)) + 
   guides(alpha = "none") + 
